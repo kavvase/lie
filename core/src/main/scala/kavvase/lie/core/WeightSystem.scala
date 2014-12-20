d69b50c045b21148eea4fdf5e66899901c4ffc55
Hidetoshi Kawase <kavvase@gmail.com>
2014-12-20 18:52:44 +0900

    computation of weights

--- core/src/main/scala/kavvase/lie/core/WeightSystem.scala
+++ core/src/main/scala/kavvase/lie/core/WeightSystem.scala
@@ -2,25 +2,59 @@ package kavvase.lie.core
 
 import kavvase.lie.core.linalg.ColVector
 
+import scala.annotation.tailrec
+
 case class Weight(
   labels: List[Int],
-  level: Int,
-  degree: Int
+  steps: List[Int]
 )
 
 class WeightSystem[G <: Lie](g: G)(implicit algebra: LieAlgebra[G]) {
 
-  import kavvase.lie.core.vector._
   import kavvase.lie.core.matrix._
+  import kavvase.lie.core.vector._
 
-  val transposedCartan = algebra.cartan(g).transpose
+  private val transposedCartan = algebra.cartan(g).transpose
 
-  def weights(highestWeight: Weight): List[Weight] = ???
-
-  def dynkinLabels(highestWeight: Weight, steps: List[Int]): List[Int] = {
-    (ColVector(highestWeight.labels) - transposedCartan * ColVector(steps)).components
+  def highestWeight(labels: List[Int]): Weight = {
+    Weight(labels, labels.map(_ => 0))
   }
 
-  def multiplicity(labels: List[Int], highestWeight: Weight): Int = ???
+  def weights(highest: Weight): Set[Weight] = {
+    val stock = updateStock(highest.labels, Map.empty)
+    loopWeights(Set((highest, stock)), highest, Set(highest))
+  }
+
+  @tailrec
+  private def loopWeights(weights: Set[(Weight, Map[Int, Int])], highest: Weight, acc: Set[Weight]): Set[Weight] = {
+    for {
+      (weight, stock) <- weights
+      next <- nextWeights(weight, stock, highest)
+    } yield next
+  } match {
+    case ws if ws.nonEmpty => loopWeights(ws, highest, acc union ws.map(_._1))
+    case _ => acc
+  }
+
+  private def nextWeights(weight: Weight, stock: Map[Int, Int], highest: Weight): Set[(Weight, Map[Int, Int])] = {
+    for {
+      (idx, count) <- stock.toSet
+      if count > 0
+      nextSteps = weight.steps.zipWithIndex.map { case (step, i) => if (i == idx) step + 1 else step }
+      labels = dynkinLabels(nextSteps, highest)
+      nextStock = updateStock(labels, stock.updated(idx, count - 1))
+      nextWeight = Weight(labels, nextSteps)
+    } yield (nextWeight, nextStock)
+  }
+
+  private def updateStock(labels: List[Int], stock: Map[Int, Int]): Map[Int, Int] = {
+    for {
+      (label, idx) <- labels.zipWithIndex
+    } yield (idx, math.max(label, stock.getOrElse(idx, 0)))
+  }.toMap
+
+  def dynkinLabels(steps: List[Int], highest: Weight): List[Int] = {
+    (ColVector(highest.labels) - transposedCartan * ColVector(steps)).components
+  }
 
 }
--- core/src/test/scala/kavvase/lie/core/WeightSystemSpec.scala
+++ core/src/test/scala/kavvase/lie/core/WeightSystemSpec.scala
@@ -6,16 +6,32 @@ class WeightSystemSpec extends Specification {
 
   "weight system" should {
 
+    "calculate weights from highest weight" in {
+      val weightSystem = new WeightSystem(A(2))
+      val highestWeight = weightSystem.highestWeight(List(1, 1))
+
+      weightSystem.weights(highestWeight) mustEqual Set(
+        Weight(labels = List(1, 1), steps = List(0, 0)),
+        Weight(labels = List(-1, 2), steps = List(1, 0)),
+        Weight(labels = List(2, -1), steps = List(0, 1)),
+        Weight(labels = List(0, 0), steps = List(1, 1)),
+        Weight(labels = List(0, 0), steps = List(1, 1)),
+        Weight(labels = List(-2, 1), steps = List(2, 1)),
+        Weight(labels = List(1, -2), steps = List(1, 2)),
+        Weight(labels = List(-1, -1), steps = List(2, 2))
+      )
+    }
+
     "calculate dynkin labels given highest weight and steps" in {
       val weightSystem = new WeightSystem(A(2))
-      val highestWeight = Weight(labels = List(1, 1), level = 0, degree = 1)
+      val highestWeight = weightSystem.highestWeight(List(1, 1))
 
-      weightSystem.dynkinLabels(highestWeight, List(1, 0)) mustEqual List(-1, 2)
-      weightSystem.dynkinLabels(highestWeight, List(0, 1)) mustEqual List(2, -1)
-      weightSystem.dynkinLabels(highestWeight, List(1, 1)) mustEqual List(0, 0)
-      weightSystem.dynkinLabels(highestWeight, List(2, 1)) mustEqual List(-2, 1)
-      weightSystem.dynkinLabels(highestWeight, List(1, 2)) mustEqual List(1, -2)
-      weightSystem.dynkinLabels(highestWeight, List(2, 2)) mustEqual List(-1, -1)
+      weightSystem.dynkinLabels(List(1, 0), highestWeight) mustEqual List(-1, 2)
+      weightSystem.dynkinLabels(List(0, 1), highestWeight) mustEqual List(2, -1)
+      weightSystem.dynkinLabels(List(1, 1), highestWeight) mustEqual List(0, 0)
+      weightSystem.dynkinLabels(List(2, 1), highestWeight) mustEqual List(-2, 1)
+      weightSystem.dynkinLabels(List(1, 2), highestWeight) mustEqual List(1, -2)
+      weightSystem.dynkinLabels(List(2, 2), highestWeight) mustEqual List(-1, -1)
     }
 
   }
